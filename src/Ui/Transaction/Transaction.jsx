ab6eeeeb257129a6d202a1bbf714769624c3f281
SufyanAli56 <sufyanalii1127@gmail.com>
2025-02-18 02:04:13 -0800

    Sufyan ali

--- src/Ui/Transaction/Transaction.jsx
+++ src/Ui/Transaction/Transaction.jsx
@@ -11,11 +11,10 @@ const Transaction = () => {
     <div>
    
       <p className='ml-52 mt-12'> 
-      MY terea Abu aa</p>
-      <h2>Jutt</h2>
-      <p>Ramiz</p>
-      <p>Ali</p>
-      <p>Saab</p>
+
+      Babaroo
+      </p>
+     
       
       
     </div>
