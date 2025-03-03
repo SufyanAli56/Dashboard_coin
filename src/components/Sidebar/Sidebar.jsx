c5a6b9b2b88a6c537642ad094bd5dbddca0ff9c4
Your Name <your-email@example.com>
2025-03-03 13:17:40 +0500

    New

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -24,7 +24,7 @@ const Sidebar = () => {
     {
       icon: <BsFillSuitDiamondFill />,
       label: "Investments",
-      path: "/investments",
+      path: "/Investments",
     },
     { icon: <LiaIdCardSolid />, label: "Insights", path: "/Insights_Main" },
     { icon: <VscSettingsGear />, label: "Promos", path: "/Promos" },
